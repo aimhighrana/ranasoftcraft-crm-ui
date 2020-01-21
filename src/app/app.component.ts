b8e096634be9228aec0309f311a34a1b5e2073e5
jsaikia <jayant.saikia@prospecta.com>
2020-01-21 12:25:53 +1100

    lib versions fixed and css lint
    Change-Id: If45ff78d75701c34b509023affed7d786ab9b6ae
    Signed-off-by: jsaikia <jayant.saikia@prospecta.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pros-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
   title = 'ngx-mdo';
