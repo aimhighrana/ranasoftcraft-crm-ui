fba3262c9ea1703e67025057be828e72d6cdcc31
Sandeep Rana <sandeep.rana@prospecta.com>
2020-12-26 10:24:17 +0530

    dataplay uri fixes ..
    Change-Id: Iec33038f5457b1a22c7b6a27caf1ca9555533708
    Signed-off-by: Sandeep Rana <sandeep.rana@prospecta.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiurl:  document.getElementsByTagName('base')[0].href.substring(0, document.getElementsByTagName('base')[0].href.indexOf('/MDOSF')),
-  dataPlalyUri:'http://dataplay.connekthub.com/api/v1.0',
+  dataPlalyUri:'https://dataplay.connekthub.com/api/v1.0',
 };
