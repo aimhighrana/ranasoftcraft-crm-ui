b034359de20a34d429dc53709b702f6d53c7b59b
vishesh.katoch <vishesh.katoch@prospecta.com>
2021-01-14 22:53:06 +0530

    Analytics MDOST 4396 and4397
    Change-Id: I66c0d7a5a45d56d9f084036cbcbcb9708ef73e54
    Signed-off-by: vishesh.katoch <vishesh.katoch@prospecta.com>

--- src/app/_modules/report/edit/container/workflowfield-control/workflowfield-control.component.spec.ts
+++ src/app/_modules/report/edit/container/workflowfield-control/workflowfield-control.component.spec.ts
@@ -5,6 +5,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MetadataModel } from '@models/schema/schemadetailstable';
 
 describe('WorkflowfieldControlComponent', () => {
   let component: WorkflowfieldControlComponent;
@@ -23,9 +24,62 @@ describe('WorkflowfieldControlComponent', () => {
     fixture = TestBed.createComponent(WorkflowfieldControlComponent);
     component = fixture.componentInstance;
   });
+// mock data
+const statics1 = [
+  {fieldId:'MATL_GROUP', fieldDescri:'Material Group'}as MetadataModel,
+  {fieldId:'MATL_TYPE', fieldDescri:'Material Type'} as MetadataModel
+]
+const statics2 = [
+  {fieldId:'MATL_GROUP', fieldDescri:'Material Group'}as MetadataModel,
+  {fieldId:'WFID', fieldDescri:'Material Type'} as MetadataModel
+]
+
+
+
+const workFlowModeleResponse = {static: statics1};
+const workFlowModeleResponse2 = {static: statics2};
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`transformFieldRes(), in TIMESERIES widget`, async(()=>{
+    // call actual method
+    component.widgetType='TIMESERIES';
+    component.controlFor='Field';
+    const actualResonse =  component.transformFieldRes(workFlowModeleResponse);
+
+    // asserts
+    expect(actualResonse.length).toEqual(1, 'length should be equlas to 1 ');
+  }));
+
+  it(`transformFieldRes(), in BAR Chart widget`, async(()=>{
+    // call actual method
+    component.widgetType='BAR_CHART';
+    const actualResonse =  component.transformFieldRes(workFlowModeleResponse2);
+
+    // asserts
+    expect(actualResonse.length).toEqual(1, 'length should be equlas to 1 ');
+  }));
+
+  it(`transformFieldRes(), in STACKED_BAR_CHART widget`, async(()=>{
+    // call actual method
+    component.widgetType='STACKED_BAR_CHART';
+    const actualResonse =  component.transformFieldRes(workFlowModeleResponse2);
+
+    // asserts
+    expect(actualResonse.length).toEqual(1, 'length should be equlas to 1 ');
+  }));
+
+  it(`transformFieldRes(), in STACKED_BAR_CHART widget`, async(()=>{
+    // call actual method
+    component.widgetType='STACKED_BAR_CHART';
+    const actualResonse =  component.transformFieldRes(workFlowModeleResponse2);
+
+    // asserts
+    expect(actualResonse.length).toEqual(1, 'length should be equlas to 1 ');
+  }));
+
+
 
 });
