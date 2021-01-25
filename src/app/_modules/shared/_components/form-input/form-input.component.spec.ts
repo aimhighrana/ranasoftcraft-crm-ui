b8e385708b098519f30dd487d0c67a9ace6c246b
smalik <shahnshah.malik@prospecta.com>
2021-01-25 20:37:49 +0530

    added test cases
    Change-Id: I53c55be63523f333db4a185c006923dcdbbcb240
    Signed-off-by: smalik <shahnshah.malik@prospecta.com>

--- src/app/_modules/shared/_components/form-input/form-input.component.spec.ts
+++ src/app/_modules/shared/_components/form-input/form-input.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormInputComponent } from './form-input.component';
 import { AppMaterialModuleForSpec } from 'src/app/app-material-for-spec.module';
+import { FormControl, Validators } from '@angular/forms';
 
 describe('FormInputComponent', () => {
   let component: FormInputComponent;
@@ -23,5 +24,20 @@ describe('FormInputComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('onBlur(), with value should update input value', async() => {
+    component.control = new FormControl('');
+    const emitBlurEventSpy = spyOn(component.emitBlurEvent, 'emit');
+    component.onBlur('updated value');
+    expect(emitBlurEventSpy).toHaveBeenCalled();
+  });
+
+  it('isRequired(), should check if the input field is required', async() => {
+    component.control = new FormControl('', Validators.required);
+    expect(component.isRequired).toBeTrue();
+
+    component.control = new FormControl('');
+    expect(component.isRequired).toBeFalse();
   });
 });
