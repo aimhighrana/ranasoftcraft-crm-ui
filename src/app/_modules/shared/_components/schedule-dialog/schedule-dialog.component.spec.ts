38f7fdadb71077b2c31eb20adcd808a797ad14f3
Z.Bilel <bilel.zheni@prospecta.com>
2021-01-26 14:41:58 +0100

    unit tests...
    Change-Id: Ib5cd92b7a1df44f60f872b8df0d5e6088881a7b8
    Signed-off-by: Z.Bilel <bilel.zheni@prospecta.com>

--- src/app/_modules/shared/_components/schedule-dialog/schedule-dialog.component.spec.ts
+++ src/app/_modules/shared/_components/schedule-dialog/schedule-dialog.component.spec.ts
@@ -5,16 +5,20 @@ import { AppMaterialModuleForSpec } from 'src/app/app-material-for-spec.module';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { ScheduleDialogComponent } from './schedule-dialog.component';
-import { SchemaScheduler } from '@models/schema/schemaScheduler';
+import { SchemaScheduler, SchemaSchedulerEnd, SchemaSchedulerRepeat, SchemaSchedulerRepeatMetric } from '@models/schema/schemaScheduler';
 import { SchemaService } from '@services/home/schema.service';
 import { of } from 'rxjs';
 import { FormInputComponent } from '../form-input/form-input.component';
 import { DatePickerFieldComponent } from '../date-picker-field/date-picker-field.component';
+import * as moment from 'moment';
 
 describe('ScheduleDialogComponent', () => {
   let component: ScheduleDialogComponent;
   let fixture: ComponentFixture<ScheduleDialogComponent>;
   let schemaService: SchemaService;
+  const mockDialogRef = {
+    close: jasmine.createSpy('close')
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -26,7 +30,7 @@ describe('ScheduleDialogComponent', () => {
         ReactiveFormsModule
       ],
       providers: [
-        { provide: MatDialogRef, useValue: {} },
+        { provide: MatDialogRef, useValue: mockDialogRef },
         { provide: MAT_DIALOG_DATA, useValue: [] },
       ]
     })
@@ -38,6 +42,8 @@ describe('ScheduleDialogComponent', () => {
     component = fixture.componentInstance;
     schemaService = fixture.debugElement.injector.get(SchemaService)
     fixture.detectChanges();
+
+    component.createForm();
   });
 
   it('should create', () => {
@@ -45,7 +51,6 @@ describe('ScheduleDialogComponent', () => {
   });
 
   it('setValue(), should set the form values by key', () => {
-    component.createForm();
     component.setValue('isEnable', true);
     expect(component.form.controls.isEnable.value).toBeTrue();
   });
@@ -54,7 +59,6 @@ describe('ScheduleDialogComponent', () => {
     const response = {
       isEnable: false
     } as SchemaScheduler;
-    component.createForm();
     component.setValueForFormControl(response);
     expect(component.form.controls.isEnable.value).toEqual(false)
   })
@@ -65,4 +69,33 @@ describe('ScheduleDialogComponent', () => {
     component.getScheduleInfo(component.schemaId);
     expect(schemaService.getSchedule).toHaveBeenCalledWith(component.schemaId);
   });
+
+  it('should getMetricHours', () => {
+    expect(component.getMetricHours).toEqual(SchemaSchedulerRepeatMetric.HOURLY);
+    component.setValue('schemaSchedulerRepeat', SchemaSchedulerRepeat.DAILY);
+    expect(component.getMetricHours).toEqual(SchemaSchedulerRepeatMetric.DAILY)
+  });
+
+  it('should submit scheduler form', () => {
+    component.submit();
+    expect(component.formSubmitted).toEqual(true);
+    expect(mockDialogRef.close).toHaveBeenCalled();
+  });
+
+  it('get getReferenceString', () => {
+    expect(component.getReferenceString).toEqual('');
+    component.setValue('end', SchemaSchedulerEnd.NEVER);
+
+    const startValue = component.form.controls.startOn.value
+    const repeatValue = component.form.controls.repeatValue.value;
+    const startStr = `starting from ${moment(parseInt(startValue, 10)).format('MM/DD/YYYY')} `;
+    const endStr = 'ending NEVER';
+
+    const finalResult = `Occurs every ${repeatValue} ${component.getMetricHours ? component.getMetricHours : ''} ${startStr} and ${endStr}`;
+
+    expect(component.getReferenceString).toEqual(finalResult);
+
+  });
+
+
 });
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -37,6 +37,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
+    browserConsoleLogOptions: {level: config.LOG_WARN},
     autoWatch: true,
     browsers: ['ChromeHeadless'],
     singleRun: true
