d5d462a2773907c9ad9872b25d6eda879a3304aa
fardim <fardim.kaiser@selise.ch>
2021-04-07 21:07:48 +0600

    quote issue

--- src/app/_modules/taskinbox/_component/task-list-datatable/task-list-datatable.component.spec.ts
+++ src/app/_modules/taskinbox/_component/task-list-datatable/task-list-datatable.component.spec.ts
@@ -1,43 +1,43 @@
-import { of } from "rxjs";
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { of } from 'rxjs';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TaskListDatatableComponent } from "./task-list-datatable.component";
-import { SharedModule } from "@modules/shared/shared.module";
-import { ActivatedRoute } from "@angular/router";
-import { RouterTestingModule } from "@angular/router/testing";
-import { AppMaterialModuleForSpec } from "src/app/app-material-for-spec.module";
+import { TaskListDatatableComponent } from './task-list-datatable.component';
+import { SharedModule } from '@modules/shared/shared.module';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppMaterialModuleForSpec } from 'src/app/app-material-for-spec.module';
 
-describe("TaskListDatatableComponent", () => {
-  let component: TaskListDatatableComponent;
-  let fixture: ComponentFixture<TaskListDatatableComponent>;
-  // let router: Router;
-  const queryParams = { s: "inbox", f: "test" };
-  const params = { node: "inbox" };
+describe('TaskListDatatableComponent', () => {
+	let component: TaskListDatatableComponent;
+	let fixture: ComponentFixture<TaskListDatatableComponent>;
+	// let router: Router;
+	const queryParams = { s: 'inbox', f: 'test' };
+	const params = { node: 'inbox' };
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [TaskListDatatableComponent],
-      imports: [AppMaterialModuleForSpec, RouterTestingModule, SharedModule],
-      providers: [
-        {
-          provide: ActivatedRoute,
-          useValue: { queryParams: of(queryParams), params: of(params) },
-        },
-      ],
-    }).compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [TaskListDatatableComponent],
+			imports: [AppMaterialModuleForSpec, RouterTestingModule, SharedModule],
+			providers: [
+				{
+					provide: ActivatedRoute,
+					useValue: { queryParams: of(queryParams), params: of(params) },
+				},
+			],
+		}).compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TaskListDatatableComponent);
-    component = fixture.componentInstance;
-    // router = TestBed.inject(Router);
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(TaskListDatatableComponent);
+		component = fixture.componentInstance;
+		// router = TestBed.inject(Router);
+		fixture.detectChanges();
+	});
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
-  it("should have node inbox", () => {
-    expect(component.node).toEqual("inbox");
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+	it('should have node inbox', () => {
+		expect(component.node).toEqual('inbox');
+	});
 });
--- src/app/_modules/taskinbox/_component/task-list-view-settings/task-list-view-settings.component.spec.ts
+++ src/app/_modules/taskinbox/_component/task-list-view-settings/task-list-view-settings.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TaskListViewSettingsComponent } from './task-list-view-settings.component';
 
 describe('TaskListViewSettingsComponent', () => {
-  let component: TaskListViewSettingsComponent;
-  let fixture: ComponentFixture<TaskListViewSettingsComponent>;
+	let component: TaskListViewSettingsComponent;
+	let fixture: ComponentFixture<TaskListViewSettingsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TaskListViewSettingsComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [TaskListViewSettingsComponent],
+		}).compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TaskListViewSettingsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(TaskListViewSettingsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
