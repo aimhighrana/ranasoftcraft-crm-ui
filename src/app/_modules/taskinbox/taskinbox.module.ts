96d97301fb84084c32e3c599e0d8f0a81720c541
fardim <fardim.kaiser@selise.ch>
2021-04-08 16:43:55 +0600

    extra component removed

--- src/app/_modules/taskinbox/taskinbox.module.ts
+++ src/app/_modules/taskinbox/taskinbox.module.ts
@@ -4,15 +4,9 @@ import { CommonModule } from '@angular/common';
 import { TaskinboxRoutingModule } from './taskinbox-routing.module';
 import { TaskListDatatableComponent } from './_component/task-list-datatable/task-list-datatable.component';
 import { SharedModule } from '@modules/shared/shared.module';
-import { TaskListViewSettingsComponent } from './_component/task-list-view-settings/task-list-view-settings.component';
-
 
 @NgModule({
-  declarations: [TaskListDatatableComponent, TaskListViewSettingsComponent],
-  imports: [
-    CommonModule,
-    TaskinboxRoutingModule,
-    SharedModule
-  ]
+  declarations: [TaskListDatatableComponent],
+  imports: [CommonModule, TaskinboxRoutingModule, SharedModule],
 })
-export class TaskinboxModule { }
+export class TaskinboxModule {}
