152c26a00f0afac5450f4485cc258c86fb3078b8
Rahul Khurana <rahul.khurana@prospecta.com>
2021-04-20 12:01:12 +0530

    Chip filter remove
    Change-Id: If0b401e20fcd5ff16449fdc7ebe474edd02e448b
    Signed-off-by: Rahul Khurana <rahul.khurana@prospecta.com>

--- src/app/_modules/list/list-routing.module.ts
+++ src/app/_modules/list/list-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from '@modules/shared/_components/page-not-found/page-not-found.component';
-import { FilterChipsComponent } from './_components/filter-chips/filter-chips.component';
 // import { ListComponent } from './_components/list.component';
 import { ListDatatableComponent } from './_components/list-datatable/list-datatable.component';
 import { ListFilterComponent } from './_components/list-filter/list-filter.component';
@@ -15,7 +14,6 @@ const routes: Routes = [
   { path: 'datatable/:moduleId', component: ListDatatableComponent },
   { path: 'table-view-settings/:moduleId/:viewId', component: TableViewSettingsComponent },
   { path: 'filter-settings/:moduleId', component: ListFilterComponent },
-  { path: 'filter-chips', component: FilterChipsComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 @NgModule({
