bdab9715a50a7e440392c24b6b50ea990919d254
Abuthahir KSM <abuthahir@prospecta.com>
2021-04-20 11:22:39 +0530

    Removing unused snackbar

--- src/app/_modules/schema/_components/v2/schema-details/schema-details.component.ts
+++ src/app/_modules/schema/_components/v2/schema-details/schema-details.component.ts
@@ -7,7 +7,6 @@ import { SchemaDataSource } from '../../schema-details/schema-datatable/schema-d
 import { SharedServiceService } from '@modules/shared/_services/shared-service.service';
 import { SchemaService } from '@services/home/schema.service';
 import { SchemaStaticThresholdRes, LoadDropValueReq, SchemaListDetails, SchemaVariantsModel, SchemaNavGrab } from '@models/schema/schemalist';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
 import { SelectionModel } from '@angular/cdk/collections';
 import { AddFilterOutput } from '@models/schema/schema';
@@ -220,7 +219,6 @@ export class SchemaDetailsComponent implements OnInit, AfterViewInit, OnChanges,
     private sharedServices: SharedServiceService,
     private schemaService: SchemaService,
     private endpointservice: EndpointsClassicService,
-    private snackBar: MatSnackBar,
     private matDialog: MatDialog,
     private schemaListService: SchemalistService,
     private schemaVariantService: SchemaVariantService,
