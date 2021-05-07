6e1784e5d1ed9005977ffbb49b2bd78897fb38ee
Z.Bilel <bilel.zheni@prospecta.com>
2021-05-07 03:35:35 +0100

    coverage thresholds
    Change-Id: I21d5b13823ae10e4e1cb810c6db6062858a6794d
    Signed-off-by: Z.Bilel <bilel.zheni@prospecta.com>

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true,
       thresholds: {
         statements: 74,
-        branches: 56.80,
+        branches: 56.69,
         functions: 70,
         lines: 74.69
       }
