901bb3231f64f3d2ab8a81a1ee2561cc411878c1
HarshitJain <Harshit.jain@prospecta.com>
2021-05-25 17:55:40 +0530

    fix

--- src/app/_modules/report/view/dashboard-container/timeseries-widget/timeseries-widget.component.ts
+++ src/app/_modules/report/view/dashboard-container/timeseries-widget/timeseries-widget.component.ts
@@ -321,7 +321,7 @@ export class TimeseriesWidgetComponent extends GenericWidgetComponent implements
   emitpanAndClickevent(startdate: string, enddate: string): void {
     if(startdate === enddate) {
       startdate = String(moment().startOf('day').toDate().getTime());
-      enddate = String(Number(startdate) + 24*60*60*1000);
+      enddate = String(moment().endOf('day').toDate().getTime());;
     }
     const fieldId = this.timeseriesData.timeSeries.groupWith;
     let appliedFilters = this.filterCriteria.filter(fill => fill.fieldId === fieldId);
