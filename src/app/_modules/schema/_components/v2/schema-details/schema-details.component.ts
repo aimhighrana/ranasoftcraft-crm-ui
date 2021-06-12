9101639e129e7f6320f373a051f48562c71395e9
gowthaman <gowthaman@prospecta.com>
2021-06-12 22:12:05 +0530

    Revert "Removed collapsible option from hierarchy menu"
    This reverts commit e408b137863db71419f9c04b1d05dfa6e0dfad7d.

--- src/app/_modules/schema/_components/v2/schema-details/schema-details.component.ts
+++ src/app/_modules/schema/_components/v2/schema-details/schema-details.component.ts
@@ -209,7 +209,7 @@ export class SchemaDetailsComponent implements OnInit, AfterViewInit, OnChanges,
 
   inlineSearchSubject: Subject<string> = new Subject();
 
-  widthOfSchemaNav = 236;
+  widthOfSchemaNav = 292;
   boxPosition: { left: number, top: number };
   public mousePosition: { x: number, y: number };
   public status: SchemaNavGrab = SchemaNavGrab.OFF;
