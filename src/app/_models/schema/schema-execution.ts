37499566105d635a5e02695df2fe055403fc1482
Abuthahir KSM <abuthahir@prospecta.com>
2021-06-14 18:00:12 +0530

    Added ruleDesc new field for schema running progress

--- src/app/_models/schema/schema-execution.ts
+++ src/app/_models/schema/schema-execution.ts
@@ -14,6 +14,7 @@ export interface SchemaExecutionProgressResponse {
     percentage: number;
     totalRules: number;
     completedRules: number;
+    ruleDesc: string;
 }
 
 export class SchemaExecutionTree {
