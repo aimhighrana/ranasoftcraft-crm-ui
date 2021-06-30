8f1e5f0bc435ad1ba9976ca05213023cfa0ddcd0
gowthaman <gowthaman@prospecta.com>
2021-06-30 23:58:46 +0530

    fixed lint issues

--- src/app/_modules/schema/_components/datascope-sidesheet/datascope-sidesheet.component.ts
+++ src/app/_modules/schema/_components/datascope-sidesheet/datascope-sidesheet.component.ts
@@ -686,7 +686,7 @@ export class DatascopeSidesheetComponent implements OnInit, OnDestroy {
       const dropdownPickLists = ['1', '30', '37'];
       if (dropdownPickLists.includes(picklist)) {
         this.filterControlType = (this.currentFilter.isCheckList === 'true') ? 'dropdown_multi' : 'dropdown_single';
-      } 
+      }
       // else if (this.dynmaicFilterSchema[picklist]) {
         // const res = this.dynmaicFilterSchema[picklist].find((x) => x.dataType === dataType.toUpperCase());
         // if (res) {
