f5a238e009a698c4c23bb350a5d471755bc6758b
Vidushi Gupta <Vidushi.gupta@unthinkable.co>
2021-07-05 10:01:34 +0530

    intergrate apis for warning list and change status

--- src/app/_modules/report/_service/report.service.ts
+++ src/app/_modules/report/_service/report.service.ts
@@ -8,7 +8,8 @@ import { EndpointsAnalyticsService } from 'src/app/_services/_endpoints/endpoint
 import { EndpointsClassicService } from '@services/_endpoints/endpoints-classic.service';
 import { ObjectTypeResponse } from '@models/schema/schema';
 import { BehaviorSubject } from 'rxjs';
-import { EmailTemplateBody, EmailRequestBody,EmailResponseBody, EmailTemplate } from '../_models/email';
+import { EmailTemplateBody, EmailRequestBody, EmailResponseBody, EmailTemplate } from '../_models/email';
+import { ImportLogs } from '../_models/import-log';
 
 @Injectable({
   providedIn: 'root'
@@ -23,41 +24,41 @@ export class ReportService {
   ) { }
 
   public getReportInfo(reportId: number, plantCode: string): Observable<any> {
-    return this.http.get<any>(this.endpointAnalyticService.reportDashboardUrl(reportId), {params: {plantCode}});
+    return this.http.get<any>(this.endpointAnalyticService.reportDashboardUrl(reportId), { params: { plantCode } });
   }
 
   public getMetaDataFldByFldIds(fieldId: string, code: string[], plantCode: string): Observable<DropDownValues[]> {
-    return this.http.post<DropDownValues[]>(this.endpointAnalyticService.getFieldMetadatByFldUrl(fieldId), code, {params: {plantCode}});
+    return this.http.post<DropDownValues[]>(this.endpointAnalyticService.getFieldMetadatByFldUrl(fieldId), code, { params: { plantCode } });
   }
 
   public createUpdateReport(request: ReportDashboardReq, plantCode: string): Observable<string> {
-    return this.http.post<string>(this.endpointAnalyticService.createUpdateReportUrl(), request, {params: {plantCode}});
+    return this.http.post<string>(this.endpointAnalyticService.createUpdateReportUrl(), request, { params: { plantCode } });
   }
 
-  public reportList(plantCode: string, roleId:string): Observable<ReportList[]> {
-    return this.http.get<ReportList[]>(this.endpointAnalyticService.getReportListUrl(), {params: {plantCode, roleId}});
+  public reportList(plantCode: string, roleId: string): Observable<ReportList[]> {
+    return this.http.get<ReportList[]>(this.endpointAnalyticService.getReportListUrl(), { params: { plantCode, roleId } });
   }
 
-  public getReportConfi(reportId: string, roleId:string): Observable<ReportList> {
-    return this.http.get<ReportList>(this.endpointAnalyticService.getReportConfigUrl(reportId), {params: {roleId}});
+  public getReportConfi(reportId: string, roleId: string): Observable<ReportList> {
+    return this.http.get<ReportList>(this.endpointAnalyticService.getReportConfigUrl(reportId), { params: { roleId } });
   }
 
   public deleteReport(reportId: string): Observable<boolean> {
     return this.http.delete<boolean>(this.endpointAnalyticService.deleteReport(reportId));
   }
 
-  public getDocCount(objectType: string, plantCode:string, isWorkflowDataset?:any, isCustomdataSet?:any): Observable<number> {
-    if(isWorkflowDataset) {
-      return this.http.get<number>(this.endpointAnalyticService.docCountUrl(objectType),{params:{plantCode, isWorkflowDataset}});
-    } else if(isCustomdataSet) {
-      return this.http.get<number>(this.endpointAnalyticService.docCountUrl(objectType),{params:{plantCode, isCustomdataSet}});
+  public getDocCount(objectType: string, plantCode: string, isWorkflowDataset?: any, isCustomdataSet?: any): Observable<number> {
+    if (isWorkflowDataset) {
+      return this.http.get<number>(this.endpointAnalyticService.docCountUrl(objectType), { params: { plantCode, isWorkflowDataset } });
+    } else if (isCustomdataSet) {
+      return this.http.get<number>(this.endpointAnalyticService.docCountUrl(objectType), { params: { plantCode, isCustomdataSet } });
     } else {
-      return this.http.get<number>(this.endpointAnalyticService.docCountUrl(objectType), {params:{plantCode}});
+      return this.http.get<number>(this.endpointAnalyticService.docCountUrl(objectType), { params: { plantCode } });
     }
   }
 
-  public getCollaboratorPermission(queryString: string, fetchCount: any) : Observable<PermissionOn> {
-    return this.http.get<PermissionOn>(this.endpointService.getAllUserDetailsUrl(),{params:{ queryString, fetchCount }});
+  public getCollaboratorPermission(queryString: string, fetchCount: any): Observable<PermissionOn> {
+    return this.http.get<PermissionOn>(this.endpointService.getAllUserDetailsUrl(), { params: { queryString, fetchCount } });
   }
 
   public getCollaboratorsPermisison(reportId: string): Observable<ReportDashboardPermission[]> {
@@ -68,8 +69,8 @@ export class ReportService {
     return this.http.post<ReportDashboardPermission[]>(this.endpointService.saveUpdateReportCollaborator(), request);
   }
 
-  public saveUpdateportDownload(request: WidgetDownloadUser[],widgetId:string, userName :string, conditionList: any): Observable<ReportDashboardPermission[]> {
-    return this.http.post<ReportDashboardPermission[]>(this.endpointAnalyticService.saveReportDownload(widgetId,userName), request, {params: {conditionList}});
+  public saveUpdateportDownload(request: WidgetDownloadUser[], widgetId: string, userName: string, conditionList: any): Observable<ReportDashboardPermission[]> {
+    return this.http.post<ReportDashboardPermission[]>(this.endpointAnalyticService.saveReportDownload(widgetId, userName), request, { params: { conditionList } });
   }
 
   public deleteCollaborator(permissionId: string): Observable<boolean> {
@@ -84,7 +85,7 @@ export class ReportService {
   public getAllLayoutsForSummary(objectType: string, wfId: string, roleId: string, plantCode: string): Observable<LayoutConfigWorkflowModel[]> {
     objectType = objectType ? objectType : '';
     wfId = wfId ? wfId : '';
-    return this.http.get<LayoutConfigWorkflowModel[]>(this.endpointService.getlayoutsUrl(),{params:{objectType,wfId, roleId, plantCode}});
+    return this.http.get<LayoutConfigWorkflowModel[]>(this.endpointService.getlayoutsUrl(), { params: { objectType, wfId, roleId, plantCode } });
   }
 
   public getCustomData(): Observable<ObjectTypeResponse[]> {
@@ -95,7 +96,7 @@ export class ReportService {
     return this.http.get<any>(this.endpointAnalyticService.getCustomDatasetFieldsUrl(objectId));
   }
 
-  public shareReport(request: EmailRequestBody, reportId:string): Observable<EmailResponseBody[]> {
+  public shareReport(request: EmailRequestBody, reportId: string): Observable<EmailResponseBody[]> {
     return this.http.put<EmailResponseBody[]>(this.endpointAnalyticService.shareReport(reportId), request);
   }
 
@@ -103,7 +104,15 @@ export class ReportService {
     return this.http.get<EmailTemplate[]>(this.endpointService.getAllTemplates());
   }
 
-  public getTemplateById(_id:string): Observable<EmailTemplateBody> {
+  public getTemplateById(_id: string): Observable<EmailTemplateBody> {
     return this.http.get<EmailTemplateBody>(this.endpointService.getTemplateById(_id));
+  }
+
+  public getImportLogList(reportId:string, page:number, size:number): Observable<ImportLogs[]> {
+    return this.http.get<ImportLogs[]>(this.endpointAnalyticService.getImportLog(reportId, page, size));
+  }
+
+  public updateImportLogStatus(messageId:string, status:string) : Observable<ImportLogs> {
+    return this.http.get<ImportLogs>(this.endpointAnalyticService.updateImportLog(messageId, status));
   }
 }
--- src/app/_modules/report/import-log/import-log.component.spec.ts
+++ src/app/_modules/report/import-log/import-log.component.spec.ts
@@ -1,21 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ImportLogComponent } from './import-log.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
 import { AppMaterialModuleForSpec } from 'src/app/app-material-for-spec.module';
 import { MdoUiLibraryModule } from 'mdo-ui-library';
+import { ReportService } from '../_service/report.service';
+import { of} from 'rxjs';
+import { ImportLogs } from '../_models/import-log';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ImportLogComponent', () => {
   let component: ImportLogComponent;
   let fixture: ComponentFixture<ImportLogComponent>;
   let router: Router;
+  let reportService: ReportService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ImportLogComponent],
-      imports: [RouterTestingModule, AppMaterialModuleForSpec, MdoUiLibraryModule],
-      providers: []
+      imports: [RouterTestingModule, AppMaterialModuleForSpec, MdoUiLibraryModule, HttpClientTestingModule],
+      providers: [ReportService]
     })
       .compileComponents();
     router = TestBed.inject(Router);
@@ -24,6 +28,7 @@ describe('ImportLogComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ImportLogComponent);
     component = fixture.componentInstance;
+    reportService = fixture.debugElement.injector.get(ReportService);
   });
 
   it('should create', () => {
@@ -46,16 +51,80 @@ describe('ImportLogComponent', () => {
   });
 
   it('getWarningList(), get warning list', () => {
-    component.dataSource = [{ warning: 'Customer Module', category: 'Missing Module', status: 'Open', updated: '12/12/20' },
-    { warning: 'BOM', category: 'Missing Module', status: 'Open', updated: '12/12/20' }]
+    const returnData: ImportLogs[] =
+      [{
+        messageId: '236823642',
+        reportId: '87347237572',
+        category: 'DUPLICATE REPORT',
+        message: 'Customer Module',
+        status: 'OPEN',
+        createdAt: '1/02/2021',
+        updatedAt: '12/04/2021',
+        updatedBy: '123'
+      },
+      {
+        message: 'Customer Module',
+        messageId: '23682364',
+        reportId: '87347237573',
+        category: 'Missing Module',
+        status: 'Open',
+        updatedAt: '12/12/20',
+        createdAt: '13/04/2020',
+        updatedBy: '123',
+      }
+      ];
+    component.reportId = '87347237573';
+    component.pageSize = 10;
+    component.currentPage = 0;
+    component.dataSource = [];
+    spyOn(reportService, 'getImportLogList').and.callFake(()=>of(returnData))
     component.initializeForm();
     component.getWarningList();
+    expect(component.dataSource.length).toEqual(2);
     expect(component.frmArray.length).toEqual(2);
+    
+    component.getWarningList(true);
+    expect(component.dataSource.length).toEqual(4);
+    expect(component.frmArray.length).toEqual(4);
   })
 
   it('change status, change status of the warning', () => {
-    component.changeStatus(0,'Open');
-    expect(component.dataSource[0].status).toEqual('Open');
+    component.dataSource =
+      [{
+        messageId: '23682364',
+        reportId: '87347237573',
+        category: 'DUPLICATE REPORT',
+        message: 'Customer Module',
+        status: 'OPEN',
+        createdAt: '1/02/2021',
+        updatedAt: '12/04/2021',
+        updatedBy: '123'
+      },
+      {
+        message: 'Customer Module',
+        messageId: '236823642',
+        reportId: '87347237573',
+        category: 'Missing Module',
+        status: 'OPEN',
+        updatedAt: '12/12/20',
+        createdAt: '13/04/2020',
+        updatedBy: '123',
+      }
+      ];
+
+      const returnData = {
+        messageId: '23682364',
+        reportId: '87347237573',
+        category: 'DUPLICATE REPORT',
+        message: 'Customer Module',
+        status: 'CLOSED',
+        createdAt: '1/02/2021',
+        updatedAt: '12/04/2021',
+        updatedBy: '123'
+      }
+    spyOn(reportService, 'updateImportLogStatus').and.callFake(()=>of(returnData))
+    component.changeStatus(0, 'CLOSED');
+    expect(component.dataSource[0].status).toEqual('CLOSED');
   })
 
 });
