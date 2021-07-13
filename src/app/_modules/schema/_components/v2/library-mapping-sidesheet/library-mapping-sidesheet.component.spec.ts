ca7872083090101f3f5086ad3ca9398b878c2e68
Abuthahir KSM <abuthahir@prospecta.com>
2021-07-13 11:15:06 +0530

    Fixed test case issues

--- src/app/_modules/schema/_components/v2/library-mapping-sidesheet/library-mapping-sidesheet.component.spec.ts
+++ src/app/_modules/schema/_components/v2/library-mapping-sidesheet/library-mapping-sidesheet.component.spec.ts
@@ -136,7 +136,7 @@ describe('LibraryMappingSidesheetComponent', () => {
     spyOn(router, 'navigate');
     component.openNounSidesheet();
     expect(router.navigate).toHaveBeenCalledWith(['', { outlets: {sb:`sb/schema/attribute-mapping/${component.moduleId}/${component.schemaId}/${component.libraryNounCode}/${component.libraryModifierCode}`,
-    outer: `outer/schema/noun/${component.moduleId}/${component.schemaId}/${component.mgroup}` }}])
+    outer: `outer/schema/noun/${component.moduleId}/${component.mgroup}` }}])
   });
 
 
@@ -226,7 +226,7 @@ describe('LibraryMappingSidesheetComponent', () => {
     component.buildMappingForm();
     component.openModifierSidesheet();
     expect(router.navigate).toHaveBeenCalledWith(['', { outlets: {sb:`sb/schema/attribute-mapping/${component.moduleId}/${component.schemaId}/${component.libraryNounCode}/${component.libraryModifierCode}`,
-    outer: `outer/schema/modifier/${component.moduleId}/${component.schemaId}/${component.mgroup}/${component.selectedNounCode}` }}])
+    outer: `outer/schema/modifier/${component.moduleId}/${component.mgroup}/${component.selectedNounCode}` }}])
   });
   it('should openAttributeSidesheet', () => {
     spyOn(router, 'navigate');
