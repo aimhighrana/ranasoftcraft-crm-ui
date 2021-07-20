c610de3ca4a8acb05fff254eefb9d7453461c76f
smalik <shahnshah.malik@prospecta.com>
2021-07-20 17:35:23 +0530

    test cases added

--- src/app/_modules/admin/_components/interfaces/interfaces.component.spec.ts
+++ src/app/_modules/admin/_components/interfaces/interfaces.component.spec.ts
@@ -2,6 +2,10 @@ import { AppMaterialModuleForSpec } from 'src/app/app-material-for-spec.module';
 import { MdoUiLibraryModule } from 'mdo-ui-library';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { InterfacesComponent } from './interfaces.component';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { FormControl } from '@angular/forms';
+import { map, startWith } from 'rxjs/operators';
+
 
 describe('InterfacesComponent', () => {
   let component: InterfacesComponent;
@@ -34,5 +38,49 @@ describe('InterfacesComponent', () => {
     component.remove('test');
     component.remove('hhh')
     expect(component.remove).toBeTruthy();
+  });
+
+  it('selected(), should select a value for autocomplete', () => {
+    component.fruitCtrl = new FormControl(null);
+      const event: MatAutocompleteSelectedEvent = {
+        option: {
+          viewValue: 'Test',
+          value: 'test'
+        }
+      } as MatAutocompleteSelectedEvent;
+      component.fruitInput = jasmine.createSpyObj('elRef', ['nativeElement']);
+      component.selected(event);
+      expect(component.fruits.length).toEqual(2);
+  });
+
+  it('remove(), should remove the passed item to selected items array', () => {
+    component.fruitCtrl = new FormControl();
+    component.fruits = [];
+    component.allFruits = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
+    const event = {
+      option: {
+        viewValue: 'Lemon'
+      },
+      source: null
+    } as MatAutocompleteSelectedEvent;
+    component.fruitInput = jasmine.createSpyObj('elRef', ['nativeElement']);
+    component.selected(event);
+    component.remove('Lemon');
+    expect(component.fruits.length).toEqual(0);
+  });
+
+  it('filter(), should returned filtered values based on the search string', () => {
+    component.fruitCtrl = new FormControl();
+    component.fruits = [];
+    component.allFruits = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
+    const values = component._filter('Lemon');
+    expect(values.length).toEqual(1);
+
+    component.fruitCtrl.valueChanges.pipe(
+      startWith(''),
+      map((num: string | null) => {
+        expect(num).toEqual('Lemon');
+      }));
+    component.fruitCtrl.setValue('Lemon');
   });
 });
