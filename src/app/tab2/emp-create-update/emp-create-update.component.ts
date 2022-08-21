27a633f739508510b05e4cbd24def6f9c5c3f7b2
sandeep.rana <sandeep.rana@prospecta.com>
2022-08-21 21:03:27 +0530

    create new emp and emp list router
    Signed-off-by: sandeep.rana <sandeep.rana@prospecta.com>
    
    Change-Id: I22e226421f5d87edd033c964ea3b906f5bcc465c

--- src/app/tab2/emp-create-update/emp-create-update.component.ts
+++ src/app/tab2/emp-create-update/emp-create-update.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-emp-create-update',
@@ -13,7 +14,8 @@ export class EmpCreateUpdateComponent implements OnInit {
 
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -21,11 +23,19 @@ export class EmpCreateUpdateComponent implements OnInit {
       empName: new FormControl('', Validators.required),
       phoneNumber: new FormControl('', Validators.required),
       address: new FormControl('', Validators.required),
+    });
+
+    this._emp.valueChanges.subscribe(v=>{
+      console.log(v);
     })
   }
 
   register() {
     console.log('clicked');
+  }
+
+  close() {
+    this.router.navigate(['afl','tabs','employee']);
   }
 
 }
--- src/app/tab2/tab2-routing.module.ts
+++ src/app/tab2/tab2-routing.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmpCreateUpdateComponent } from './emp-create-update/emp-create-update.component';
+import { ListComponent } from './list/list.component';
 import { Tab2Page } from './tab2.page';
 
 const routes: Routes = [
   {
     path: '',
     component: Tab2Page,
-  },{
-    path: 'new',
-    component: EmpCreateUpdateComponent,
+    children:[
+      {
+        path:'list',
+        component: ListComponent
+      },{
+        path: 'new',
+        component: EmpCreateUpdateComponent,
+      },
+      {
+        path:'',
+        redirectTo:'list',
+        pathMatch:'full'
+      }
+    ]
   }
 ];
 
